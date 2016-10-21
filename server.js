9614d7f8d54cff76405a6c19af8edc64bd8051be
nikhil2288 <nikhil.sharma2288@gmail.com>
2016-10-21 14:13:47 +0530

    Initial Set Up For mean chirp

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+
+// modules =================================================
+var express        = require('express');
+var app            = express();
+
+// set our port
+var port = process.env.PORT || 8080;
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 
+
+// routes ==================================================
+require('./server/routes')(app); // configure our routes
+
+// start app ===============================================
+// startup our app at http://localhost:8080
+app.listen(port);               
+
+// shoutout to the user                     
+console.log('Magic happens on port ' + port);
+
+// expose app           
+exports = module.exports = app;                         
